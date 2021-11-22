65c68b7c149e1dba11d71f76896a27934440b47b
jcrump88 <jcrump88@gmail.com>
2021-11-22 16:58:21 -0600

    started activity-6

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.linq;
 using System.Threading.Tasks;
 using Application.Posts;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Persistence;
 
 namespace API.Controllers
 {
@@ -15,7 +17,31 @@ namespace API.Controllers
     {
         private readonly IMediator mediator;
 
+        private readonly DataContext context;
+        
         public PostsController(IMediator mediator) => this.mediator = mediator;
+        
+        /// <summary>
+        ///  GET api/posts
+        /// </summary>
+        /// <returns>A List of posts</returns>
+        [HttpGet]
+        public ActionResult<list<post>> Get()
+        {
+            return this.context.Posts.ToList();
+        }
+
+        /// <summary>
+        /// GET api/post/[id]
+        /// </summary>
+        /// <param name="id">Post id</param>
+        /// <returns>A single post</returns>
+        [HttpGet("{id}")]
+
+        public ActionResult<Post> GetById(Guid id)
+        {
+            return this.context.Posts.Find(id);
+        }
 
         public async Task<ActionResult<List<Post>>> List()
         {
@@ -23,4 +49,6 @@ namespace API.Controllers
         }
 
     }
+
+  
 }
