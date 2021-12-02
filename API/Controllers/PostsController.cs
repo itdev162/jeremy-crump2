63cc41fc44bcd9cdae9c8df9f5cd93b4c3bd06cf
jcrump88 <jcrump88@gmail.com>
2021-12-01 18:38:08 -0600

    Added create post

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.linq;
+using System.Linq;
 using System.Threading.Tasks;
 using Application.Posts;
 using Domain;
@@ -30,26 +30,38 @@ namespace API.Controllers
             return await this.mediator.Send(new List.Query());
         }
 
-        /// <summary>
-        ///  GET api/posts
-        /// </summary>
-        /// <returns>A List of posts</returns>
         [HttpGet]
-        public ActionResult<list<post>> Get()
+        public ActionResult<List<Post>> Get()
         {
             return this.context.Posts.ToList();
         }
 
-        /// <summary>
-        /// GET api/post/[id]
-        /// </summary>
-        /// <param name="id">Post id</param>
-        /// <returns>A single post</returns>
         [HttpGet("{id}")]
 
         public ActionResult<Post> GetById(Guid id)
         {
             return this.context.Posts.Find(id);
+        }
+
+        public ActionResult <Post> Create([FromBody] Post request)
+        {
+            var post = new Post 
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if(success)
+            {
+                return post;
+            }
+
+            throw new Exception("Error creating post");
         }
 
     }
